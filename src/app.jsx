9500775a97542eb55094c07c80bc800c7d4fa4e6
LuksFay <luksfaydev@gmail.com>
2022-01-14 03:13:38 -0300

    state

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props){ 
-  return(
-    <div>
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date:new Date()};
+  }
+  
+  componentDidMount(){
+    this.timerID = setInterval(
+      ()=> this.tick(),1000
+    );
+  }
+  
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render(){
+    return(
+        <div>
       <h1>Hello, World!</h1>
-      <h2> It is: {props.date.toLocaleTimeString()}.
+      <h2> It is: {this.state.date.toLocaleTimeString()}.
       </h2>
     </div>
-  );
-  
+    );
+  }
 }
 
 class App extends React.Component {	
   render() {
     return (
-     <Clock date={new Date()}/>
+     <Clock />
     )
   }
 }
-function tick(){
   ReactDOM.render(<App />, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
